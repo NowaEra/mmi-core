7a03c1d18e29389476aab368d41fa3dfa562b772
Marcin Bonk <marvcin@gmail.com>
2020-01-20 14:15:13 +0100

    Kernel extending

--- src/App/AbstractKernel.php
+++ src/App/AbstractKernel.php
@@ -3,41 +3,105 @@ declare(strict_types=1);
 
 namespace Mmi\App;
 
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
-use Symfony\Component\HttpKernel\Kernel as HttpKernel;
 
 /**
  * Class AbstractKernel
  * @package Mmi\App
  */
-abstract class AbstractKernel extends HttpKernel
+abstract class AbstractKernel
 {
+    const VERSION = "1.0.0";
+
+    /** @var string */
+    protected $env;
+
+    /** @var bool */
+    protected $debug;
+
+    /** @var bool */
+    protected $booted = false;
+
+    public function __construct(string $env, bool $debug = false)
+    {
+        $this->env   = $env;
+        $this->debug = $debug;
+    }
+
+    public function boot(): void
+    {
+        if (true === $this->booted) {
+            return;
+        }
+        $builder = $this->getContainerBuilder();
+        $builder->getParameterBag()->add($this->getKernelParameters());
+        $this->dumpContainer();
+
+        $this->booted = true;
+    }
+
+    private function dumpContainer(): void
+    {
+        $file = $this->getCacheDir() . '/' . $this->env . '/container.php';
+        if (false === $this->debug && file_exists($file)) {
+            require_once $file;
+            $container = new MmiCachedContainer();
+        } else {
+            $containerBuilder = new ContainerBuilder();
+            // ...
+            $containerBuilder->compile();
+
+            if (false === $this->debug) {
+                $dumper = new PhpDumper($containerBuilder);
+                file_put_contents(
+                    $file,
+                    $dumper->dump(['class' => 'MmiCachedContainer'])
+                );
+            }
+        }
+    }
+
+    protected function getContainerBuilder(): ContainerBuilder
+    {
+        $builder = new ContainerBuilder();
+
+        return $builder;
+    }
+
+    protected function getCacheDir(): string
+    {
+        return (realpath(
+                $this->getProjectDir()
+            ) ?: $this->getProjectDir()) . '/var/cache';
+    }
+
+    protected function getKernelParameters(): array
+    {
+        return [
+            'kernel.project_dir' => realpath(
+                $this->getProjectDir()
+            ) ?: $this->getProjectDir(),
+            'kernel.environment' => $this->env,
+            'kernel.debug'       => $this->debug,
+            'kernel.cache_dir'   => $this->getCacheDir(),
+        ];
+    }
+
     /**
      * @return ExtensionInterface[]
      */
     abstract protected function getExtensions(): array;
 
     /**
-     * @param ContainerBuilder $container
+     * @return CompilerPassInterface[]
      */
-    protected function prepareContainer(ContainerBuilder $container)
-    {
-        foreach ($this->getExtensions() as $extension) {
-            $container->registerExtension($extension);
-        }
-
-        parent::prepareContainer($container);
-    }
+    abstract protected function getCompilerPasses(): array;
 
     /**
-     * Returns an array of bundles to register.
-     *
-     * @return iterable|BundleInterface[] An iterable of bundle instances
+     * @return string
      */
-    public function registerBundles()
-    {
-        return [];
-    }
+    abstract protected function getProjectDir(): string;
 }
