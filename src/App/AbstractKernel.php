e637360d3311934c5d4d811f9ca3bec950757c61
Marcin Bonk <marvcin@gmail.com>
2020-01-21 05:20:08 +0100

    Cleared unused stuff in favor of http-kernel

--- src/App/AbstractKernel.php
+++ src/App/AbstractKernel.php
@@ -3,41 +3,17 @@ declare(strict_types=1);
 
 namespace Mmi\App;
 
-use Mmi\DependencyInjection\CoreExtension;
-use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Class AbstractKernel
  * @package Mmi\App
  */
-abstract class AbstractKernel
+abstract class AbstractKernel extends Kernel
 {
-    const VERSION = "1.0.0";
-
-    /** @var string */
-    protected $env;
-
-    /** @var bool */
-    protected $debug;
-
-    /** @var bool */
-    protected $booted = false;
-
-    /** @var Container */
-    protected $container;
-
-    public function __construct(string $env, bool $debug = false)
-    {
-        $this->env   = $env;
-        $this->debug = $debug;
-    }
 
     /**
      * @return ExtensionInterface[]
@@ -50,78 +26,18 @@ abstract class AbstractKernel
     abstract protected function getCompilerPasses(): array;
 
     /**
-     * @return string
+     * @param ContainerBuilder $container
      */
-    abstract protected function getProjectDir(): string;
-
-    protected function boot(): void
+    protected function build(ContainerBuilder $container): void
     {
-        if (true === $this->booted) {
-            return;
+        foreach ($this->getExtensions() as $extension) {
+            $container->registerExtension($extension);
         }
-        $this->dumpContainer();
 
-        $this->booted = true;
-    }
-
-    private function prepareContainer(ContainerBuilder $builder): void
-    {
-        $builder->registerExtension(new CoreExtension());
-        $builder->getParameterBag()->add($this->getKernelParameters());
-        $builder->set('kernel', $this);
-    }
-
-    private function dumpContainer(): void
-    {
-        $file = $this->getCacheDir() . '/' . $this->env . '/container.php';
-        if (false === is_dir(dirname($file))) {
-            mkdir(dirname($file), 0777, true);
+        foreach ($this->getCompilerPasses() as $compilerPass) {
+            $container->addCompilerPass($compilerPass);
         }
-        $containerConfigCache = new ConfigCache($file, $this->debug);
-        if (false === $containerConfigCache->isFresh()) {
-            $containerBuilder = new ContainerBuilder();
-            $this->prepareContainer($containerBuilder);
-            $containerBuilder->compile();
 
-            $dumper = new PhpDumper($containerBuilder);
-            $containerConfigCache->write(
-                $dumper->dump(['class' => 'MmiCachedContainer']),
-                $containerBuilder->getResources()
-            );
-        }
-        require_once $file;
-        $this->container = new \MmiCachedContainer();
-    }
-
-    protected function getCacheDir(): string
-    {
-        return (realpath(
-                $this->getProjectDir()
-            ) ?: $this->getProjectDir()) . '/var/cache';
-    }
-
-    protected function getKernelParameters(): array
-    {
-        return [
-            'kernel.project_dir' => realpath(
-                $this->getProjectDir()
-            ) ?: $this->getProjectDir(),
-            'kernel.environment' => $this->env,
-            'kernel.debug'       => $this->debug,
-            'kernel.cache_dir'   => '%kernel.project_dir%/var/cache',
-            'kernel.version'     => self::VERSION
-        ];
-    }
-
-    protected function getHttpKernel(): HttpKernel
-    {
-        return $this->container->get(HttpKernel::class);
-    }
-
-    public function handle(Request $request): Response
-    {
-        $this->boot();
-
-        return $this->getHttpKernel()->handle($request);
+        parent::build($container);
     }
 }
